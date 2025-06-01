0edbc2b40953c261b290b1167fad03a66ff89db6
Antelzcreate <antelztheresasunny@gmail.com>
2025-06-01 10:20:08 +0300

    Added GroupBox, button, and wave drawing function

--- GitNetUzdevums/Form1.cs
+++ GitNetUzdevums/Form1.cs
@@ -77,5 +77,46 @@ namespace WindowsFormsApplication_15
                 x += step;
             }
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            
+            {
+                
+                if (pictureBox1.Image != null)
+                    pictureBox1.Image.Dispose();
+
+                // Create a blank image to draw on
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics g = Graphics.FromImage(bmp);
+
+                double m = 30;
+                int xc = pictureBox1.Width / 2;
+                int yc = pictureBox1.Height / 2;
+                double step = 0.01;
+
+                Pen p = new Pen(Color.Purple, 2);
+
+                for (double x = -Math.PI; x <= Math.PI; x += step)
+                {
+                    double y = Math.Sin(x);
+                    int xe = (int)(xc + x * m);
+                    int ye = (int)(yc - y * m);
+                    g.DrawEllipse(p, xe, ye, 1, 1);
+                }
+
+                // Set the drawing to the PictureBox
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        }
     }
-}
+
